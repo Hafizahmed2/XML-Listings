b97e4030ede100869724a99c936db81f0192f713
“Hafizahmed2” <hafizahmed.tx@gmail.com>
2022-11-23 22:49:50 +0500

    delete table

--- includes/class-listing_xml-deactivator.php
+++ includes/class-listing_xml-deactivator.php
@@ -20,7 +20,8 @@
  * @subpackage Listing_xml/includes
  * @author     Ahmed <hafizahmed.tx@gmail.com>
  */
-class Listing_xml_Deactivator {
+class Listing_xml_Deactivator
+{
 
 	/**
 	 * Short Description. (use period)
@@ -29,8 +30,12 @@ class Listing_xml_Deactivator {
 	 *
 	 * @since    1.0.0
 	 */
-	public static function deactivate() {
-		
+	public static function deactivate()
+	{
+		global $wpdb;
+     $table_name = $wpdb->prefix . 'listing_xml';
+     $sql = "DROP TABLE IF EXISTS $table_name";
+     $wpdb->query($sql);
+     delete_option("my_plugin_db_version");
 	}
-
 }
